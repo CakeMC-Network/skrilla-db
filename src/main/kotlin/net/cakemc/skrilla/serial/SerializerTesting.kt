9624216469a3d193324f31f8a0143f94de02e943
u64lisa <106406816+u64lisa@users.noreply.github.com>
2025-04-12 00:41:50 +0200

    :sparkle: speed displaying for serial

--- src/main/kotlin/net/cakemc/skrilla/serial/SerializerTesting.kt
+++ src/main/kotlin/net/cakemc/skrilla/serial/SerializerTesting.kt
@@ -8,9 +8,13 @@ fun main() {
     val person = Person("Alice", 30, true,
         UUID.randomUUID(), Date(), arrayOf("tag1", "tag2"))
 
+    var start = System.currentTimeMillis()
     val serializedData = serializer.serialize(person)
+    println("serialization took ${System.currentTimeMillis()-start} ms")
 
+    start = System.currentTimeMillis()
     val deserializedPerson = serializer.deserialize(serializedData, Person::class.java)
+    println("deserialization took ${System.currentTimeMillis()-start} ms")
 
     println("Original person: $person")
     println("Deserialized person: $deserializedPerson")
